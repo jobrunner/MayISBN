03b2a28478b51339b0e671706fce6ae56e5703db
Jo Brunner <jo.brunner@mayflower.de>
2017-06-10 11:27:04 +0200

    init

---
+++ MayISBN/MayISBN.h
@@ -0,0 +1,19 @@
+//
+//  MayISBN.h
+//  MayISBN
+//
+//  Created by Jo Brunner on 10.06.17.
+//  Copyright © 2017 Jo Brunner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MayISBN.
+FOUNDATION_EXPORT double MayISBNVersionNumber;
+
+//! Project version string for MayISBN.
+FOUNDATION_EXPORT const unsigned char MayISBNVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MayISBN/PublicHeader.h>
+
+
---
+++ MayISBNTests/MayISBNTests.m
@@ -0,0 +1,39 @@
+//
+//  MayISBNTests.m
+//  MayISBNTests
+//
+//  Created by Jo Brunner on 10.06.17.
+//  Copyright © 2017 Jo Brunner. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MayISBNTests : XCTestCase
+
+@end
+
+@implementation MayISBNTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
